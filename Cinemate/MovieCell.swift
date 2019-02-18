3ba95560a98374335ddd37be058a021ac414a829
Kunal Agarwal <kunal.ajay@gmail.com>
2019-02-18 15:24:17 -0800

    Minor fixes

---
+++ Cinemate/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Cinemate
+//
+//  Created by Kunal Agarwal on 18/02/19.
+//  Copyright © 2019 Kunal. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
