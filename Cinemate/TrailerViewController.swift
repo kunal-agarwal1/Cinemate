2647d1c26dcfcb2b6297c345ae870a0cc65cfad5
Kunal Agarwal <kunal.ajay@gmail.com>
2019-02-26 00:49:43 -0800

    Pre final commit

--- Cinemate/TrailerViewController.swift
+++ Cinemate/TrailerViewController.swift
@@ -12,24 +12,51 @@ import WebKit
 class TrailerViewController: UIViewController, WKNavigationDelegate {
 
     var key: String!
-    var webView: WKWebView!
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let URLstring = "https://www.youtube.com/watch?v="+key
-        print(URLstring)
-        let url = URL(string: URLstring)!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+        if(key == nil || key == "no_trailer"){
+            view = UIView()
+            let alertController = UIAlertController(title: "iOScreator", message:
+                "Trailer not found", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+        else{
+        let urlString = "https://www.youtube.com/watch?v="+key
+        print(urlString)
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        let request = URLRequest(url: URL(string: urlString)!)
+        self.webView.load(request)
+       // self.webView.allowsBackForwardNavigationGestures = true
+        self.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
+       // self.view.bringSubviewToFront(webView1)
         // Do any additional setup after loading the view.
+        }
+        
     }
-    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            if webView.isLoading {
+                activityIndicator.startAnimating()
+                activityIndicator.isHidden = false
+            } else {
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
 
+    @IBAction func dismissClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
     /*
     // MARK: - Navigation
 
